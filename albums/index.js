2cef2c94a1f469ddb87add6bc03c79883faa0c85
Konesta <Konsta.suutari@gmail.com>
2018-05-21 22:21:09 +0300

    headeri
    header made and axios installed.

--- albums/index.js
+++ albums/index.js
@@ -1,11 +1,15 @@
 // Import a library to help create a Component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
-
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a Component
 const App = () => (
-  <Text>Tekstiä</Text>
+  <View>
+  <Header headerText={'Albums'} />
+  <AlbumList />
+  </View>
   );
 
 // Render it to the device
