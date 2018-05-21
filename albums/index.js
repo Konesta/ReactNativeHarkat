897c9d91975ad16355e605c4cdb74e4230ee17e1
Konesta <Konsta.suutari@gmail.com>
2018-05-21 20:09:49 +0300

    project setup
    Project setup, along with necessary eslint stuff.

---
+++ albums/index.js
@@ -0,0 +1,12 @@
+// Import a library to help create a Component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+
+// Create a Component
+const App = () => (
+  <Text>Tekstiä</Text>
+  );
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
